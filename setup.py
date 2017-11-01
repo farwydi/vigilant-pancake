edfd80bf86efe90723b257c455b0ef29d9b9afdb
Leonid Zharikov <leon.zharikov@gmail.com>
2017-11-01 16:31:53 +0300

    move to src

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='vigilant-pancake',
       version='0.1',
       description='The game for learning neural network.',
-      url='http://github.com/icddex/vigilant-pancake',
+      url='http://github.com/icddex/src',
       author='Zharikov Leonid',
       author_email='leon.zharikov@gmail.com',
       license='MIT',
-      packages=['vigilant-pancake'],
+      packages=['src'],
       zip_safe=False)
