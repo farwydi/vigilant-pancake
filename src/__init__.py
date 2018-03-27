8eed6fad0baa4d181d77fb86b550923db28e43a9
Leonid Zharikov <leon.zharikov@gmail.com>
2018-03-27 12:44:43 +0300

    version 2

--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-try:  # pragma: no cover
-    import cv2
-except ImportError:  # pragma: no cover
-    raise ImportError("Please install OpenCV - pip install opencv-python")
-
-try:  # pragma: no cover
-    import numpy
-except ImportError:  # pragma: no cover
-    raise ImportError("Please install numpy - pip install numpy")
---
+++ src/games.py
@@ -0,0 +1,56 @@
+import time
+
+import pygame
+
+import config
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+pygame.init()
+
+screen = pygame.display.set_mode(config.DISPLAY_SIZE)
+
+clock = pygame.time.Clock()
+done = False
+
+
+class LiveObject(object):
+    pass
+
+
+class AI(LiveObject):
+    pass
+
+
+score = 0
+
+mgScore = pygame.font.SysFont('Comic Sans MS', 60)
+
+while not done:
+    start_time = time.time()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    screen.fill(WHITE)
+
+    fps = 1.0 / (time.time() - start_time)
+    score_text = mgScore.render("FPS: " + str(round(fps, 2)), False, (0, 0, 0))
+    screen.blit(score_text, (20, 0))
+
+    for i in range(config.COUNT_LINE_OBJECT):
+        for j in range(config.COUNT_LINE_OBJECT):
+            x, y = (i * config.PLANCK_LENGTH, j * config.PLANCK_LENGTH)
+            h, w = (config.PLANCK_LENGTH, config.PLANCK_LENGTH)
+
+            pygame.draw.rect(screen, RED, (x, config.HEADER_SIZE + y, h, w), 1)
+
+    score += 1
+
+    pygame.display.flip()
+
+pygame.quit()
