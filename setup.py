bcc23e0d11d9ed20b132040640e4898d4fa4529c
Leonid Zharikov <leon.zharikov@gmail.com>
2018-03-27 12:47:48 +0300

    version 2 update

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='vigilant-pancake',
-      version='0.1',
+      version='2.0',
       description='The game for learning neural network.',
-      url='http://github.com/icddex/src',
+      url='http://github.com/farwydi/src',
       author='Zharikov Leonid',
       author_email='leon.zharikov@gmail.com',
       license='MIT',
