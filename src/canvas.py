4743696f0b4db40e893e01fe270a29c2dc0bcdc5
Leonid Zharikov <leon.zharikov@gmail.com>
2018-03-28 19:08:59 +0300

    update

---
+++ src/canvas.py
@@ -0,0 +1,49 @@
+from random import randint
+
+from numpy import zeros, clip
+
+import config
+
+grid = zeros((config.COUNT_LINE_OBJECT, config.COUNT_LINE_OBJECT), dtype=int)
+
+
+def fill_grid(type_object, func_fill, count):
+    it = count
+    while it > 0:
+        ri, rj = randint(0, config.COUNT_LINE_OBJECT - 1), randint(0, config.COUNT_LINE_OBJECT - 1)
+
+        if grid[ri][rj] != 0:
+            continue
+
+        if grid[ri][rj] == type_object:
+            continue
+
+        func_fill(type_object, ri, rj)
+        it -= 1
+
+
+def simple_fill(type_object, ri, rj):
+    grid[ri][rj] = type_object
+
+
+def well_fill(type_object, ri, rj):
+    direction = randint(0, 3)
+
+    for length in range(randint(1, config.MAX_WALL_LENGTH)):
+        if direction == 0:
+            grid[clip(ri + length, 0, config.COUNT_LINE_OBJECT - 1)][rj] = type_object
+        elif direction == 1:
+            grid[clip(ri - length, 0, config.COUNT_LINE_OBJECT - 1)][rj] = type_object
+        elif direction == 2:
+            grid[ri][clip(rj + length, 0, config.COUNT_LINE_OBJECT - 1)] = type_object
+        elif direction == 3:
+            grid[ri][clip(rj - length, 0, config.COUNT_LINE_OBJECT - 1)] = type_object
+
+
+fill_grid(1, simple_fill, config.MAX_EAT_COUNT)
+fill_grid(2, simple_fill, config.MAX_POISON_COUNT)
+fill_grid(3, well_fill, config.MAX_WALL_COUNT)
+
+live_x = 23
+live_y = 23
+grid[live_x][live_y] = 4
--- src/games.py
+++ src/games.py
@@ -1,13 +1,42 @@
+import random
 import time
 
+import numpy
 import pygame
 
 import config
+from canvas import grid, live_x, live_y
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
+YELLOW = (0, 255, 255)
+BLUE = (0, 0, 255)
+
+color = {
+    0: WHITE,
+    1: GREEN,
+    2: YELLOW,
+    3: BLUE,
+    4: RED
+}
+
+object_color = {
+    0: WHITE,
+    1: GREEN,  # EAT
+    2: RED,  # POISON
+    3: YELLOW,  # WALL
+    4: BLUE  # PLAYER
+}
+
+object_fill = {
+    0: 0,
+    1: 1,  # EAT
+    2: 1,  # POISON
+    3: 0,  # WALL
+    4: 0  # PLAYER
+}
 
 pygame.init()
 
@@ -33,6 +62,35 @@ class AI(LiveObject):
         self.type = 1
 
 
+def move(grid, i: int, j: int, vertical: int, horizontal: int):
+    if grid[i][j] != 4:
+        raise Exception()
+
+    vertical = numpy.clip(vertical, -1, 1)
+    vertical = i + vertical
+    if vertical >= config.COUNT_LINE_OBJECT:
+        vertical = 0
+
+    if vertical < -config.COUNT_LINE_OBJECT:
+        vertical = config.COUNT_LINE_OBJECT - 1
+
+    horizontal = numpy.clip(horizontal, -1, 1)
+    horizontal = horizontal + j
+    if horizontal >= config.COUNT_LINE_OBJECT:
+        horizontal = 0
+
+    if horizontal < -config.COUNT_LINE_OBJECT:
+        horizontal = config.COUNT_LINE_OBJECT - 1
+
+    if grid[vertical][horizontal] == 0:
+        grid[vertical][horizontal] = 4
+        grid[i][j] = 0
+
+        return vertical, horizontal
+
+    return i, j
+
+
 score = 0
 
 mgScore = pygame.font.SysFont('Comic Sans MS', 60)
@@ -46,16 +104,26 @@ while not done:
 
     screen.fill(WHITE)
 
-    fps = 1.0 / (time.time() - start_time)
+    try:
+        fps = 1.0 / (time.time() - start_time)
+    except ZeroDivisionError:
+        fps = 1.0
+
     score_text = mgScore.render("FPS: " + str(round(fps, 2)), False, (0, 0, 0))
     screen.blit(score_text, (20, 0))
 
+    live_x, live_y = move(grid, live_x, live_y, random.randint(-1, 1), random.randint(-1, 1))
+
     for i in range(config.COUNT_LINE_OBJECT):
         for j in range(config.COUNT_LINE_OBJECT):
+            object_type = grid[i][j]
+            if object_type == 0:
+                continue
             x, y = (i * config.PLANCK_LENGTH, j * config.PLANCK_LENGTH)
             h, w = (config.PLANCK_LENGTH, config.PLANCK_LENGTH)
 
-            pygame.draw.rect(screen, RED, (x, config.HEADER_SIZE + y, h, w), 1)
+            pygame.draw.rect(screen, object_color[object_type], (x, config.HEADER_SIZE + y, h, w),
+                             object_fill[object_type])
 
     score += 1
 
